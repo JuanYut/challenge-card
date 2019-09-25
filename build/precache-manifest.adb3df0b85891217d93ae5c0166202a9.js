7c9d9fdebe358d3b960b5ea074b191b83b1d5b66
JuanYut <juanyut97@hotmail.com>
2019-09-24 22:05:26 -0500

    build again lol

---
+++ build/precache-manifest.adb3df0b85891217d93ae5c0166202a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b7a954c6f11ee26bdc7f6f2bdb4e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c6d7bcc1704b3395c3",
+    "url": "/static/css/main.05f6a1a8.chunk.css"
+  },
+  {
+    "revision": "9f2750530b64b2ec2963",
+    "url": "/static/js/2.9ea1c6ad.chunk.js"
+  },
+  {
+    "revision": "56c6d7bcc1704b3395c3",
+    "url": "/static/js/main.fce3a58b.chunk.js"
+  },
+  {
+    "revision": "ff45a8898108376c1763",
+    "url": "/static/js/runtime-main.389e6afb.js"
+  }
+]);
